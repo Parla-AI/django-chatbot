c39a7a68624316f107f817ddab4fddfdc309692c
unknown <vinci22>
2024-09-17 14:09:44 -0500

    added statis fileds

--- django_chatbot/django_chatbot/settings.py
+++ django_chatbot/django_chatbot/settings.py
@@ -28,7 +28,9 @@ DEBUG = False
 ALLOWED_HOSTS = ["*"]
 # CSRF_TRUSTED_ORIGINS = ["http://*"]
 
-
+CSRF_TRUSTED_ORIGINS = [
+'https://*.railway.app'
+]
 # Application definition
 
 INSTALLED_APPS = [
