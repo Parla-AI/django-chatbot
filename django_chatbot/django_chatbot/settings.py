25b3875a358f91305ee13661bb94400596d30388
unknown <vinci22>
2024-12-05 01:18:07 -0500

    added  branch ofr code shopy

--- django_chatbot/django_chatbot/settings.py
+++ django_chatbot/django_chatbot/settings.py
@@ -128,4 +128,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
-STATIC_ROOT =os.pain(BASE_DIR, 'templates')th.jo
+STATIC_ROOT =os.path.join(BASE_DIR, 'templates')
